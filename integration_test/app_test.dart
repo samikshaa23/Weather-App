3ceb131205b1f2f1184dfde9cf6c04e3292210e8
samikshaa23 <samiksha.mohite@softdel.com>
2024-01-31 16:20:46 +0530

    edit integration test command

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+﻿import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'login_test.dart';
